32b8470723593378a5bcb774f766a2a195e28ac0
cappAndreas <79117677+cappAndreas@users.noreply.github.com>
2024-05-31 08:44:30 +0200

    Update hoodie.cpp

--- hoodie.cpp
+++ hoodie.cpp
@@ -20,9 +20,9 @@ class CfgVehicles
 	class Hoodie_ColorBase;		// Make sure to call the right class for the item you are retexturing
 	class custom_hoodie: Hoodie_ColorBase	// Change custom_hoodie to whatever class name you want for your new item.
 	{
-        displayName="Whatever you want the hoodie to be called ingame";
-        descriptionShort="ingame description of the hoodie";
-        scope=2;
+        	displayName="Whatever you want the hoodie to be called ingame";
+        	descriptionShort="ingame description of the hoodie";
+        	scope=2;
 		attachments[]={"Shoulder","magazine"}; // All slots can be found in P:\scripts\config.cpp
 		itemsCargoSize[]={10,100};		// 10-Horizontal, 100-Vertical = 100 slots in inventory. H can be 1-10, V 1-100
 		itemSize[]={2,1};		// How many slots the item will use if placed in inventory
